1eac1fe1657ea4ce13224cce9e637fbb23c5b8db
cfreeptr0 <c@freeptr.com>
2019-08-24 17:49:24 -0400

    Stuffs and things

--- myGui/muh_gui.java
+++ myGui/muh_gui.java
@@ -1,16 +1,7 @@
 Public class myGui implements Comparable<String>{
-    private String myString;
-
-    public myGui() {
-        mySting = new String("a string");
-    }
-
-    public String getString() {
-	return this.myString;
-    }
-
+    private String myStrung;
+    public String getString() {return this.myStrung;}
     @Override
-    public int compareTo(String comparable) {
-	return this.myString.compareTo(comparable);
-    }
+    public int compareTo(String comparable) {return this.myString.compareTo(comparable);}
+    public myGui() {myStrung = new String("this is a really big strung");}
 }
