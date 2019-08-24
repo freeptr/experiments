c6b0cae7268b0da4bc5af009a1a90f1fa57f9a53
Frank Perez <frankperez@Franks-MacBook-Pro.local>
2019-08-24 17:27:14 -0400

    Add a compareTo method to compare against a string.
    Implements the comparable interface so that we can compare this object to String type objects.

--- myGui/myGui.java
+++ myGui/myGui.java
@@ -1,4 +1,4 @@
-Public class myGui {
+Public class myGui implements Comparable<String>{
     private String myString;
 
     public myGui() {
@@ -7,5 +7,10 @@ Public class myGui {
 
     public String getString() {
 	return this.myString;
+    }
+
+    @Override
+    public int compareTo(String comparable) {
+	return this.myString.compareTo(comparable);
     }
 }
