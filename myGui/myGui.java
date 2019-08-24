48b120239995d66b37f82d2556c8c93e195dd4c7
Frank Perez <frankperez@Franks-MacBook-Pro.local>
2019-08-24 16:32:11 -0400

    Add the myGui package and the myGui class.
    This is a package that does gui so that we can have more gui.  The my gui class does the gui that needs to be done so there is more guiness.Add the myGui package and the myGui class.

---
+++ myGui/myGui.java
@@ -0,0 +1,11 @@
+Public class myGui {
+    private String myString;
+
+    public myGui() {
+        mySting = new String("a string");
+    }
+
+    public String getString() {
+	return this.myString;
+    }
+}
