2a1cd21c77d0a34552c3a226b6d9bb941f4d5562
dustinbcox <dustinbcox@gmail.com>
2019-08-24 16:53:39 -0400

    new stuff

---
+++ myGui/muh_gui.java
@@ -0,0 +1,14 @@
+Public class myGui {
+    public String getString() {
+
+	return this.myString;
+    }
+
+    public myGui() {
+        mySting = new String("a aslkdfjaslkdjfasl;kdfjas;lkdfjaslkdfjalskdjflaksjdflkasdjflksadjflkasdjflkasjdflkasjdflkasdjlkasdfjstr drugs are cool ing");
+    }
+// late night last night
+// :-(
+
+    private String myString;
+}
